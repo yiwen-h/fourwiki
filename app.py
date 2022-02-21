e7f9b11508fddef0b770ab4384ba831407ceabf5
YiWen Hon <yiwench@gmail.com>
2022-02-21 19:00:34 +0000

    edited button

--- app.py
+++ app.py
@@ -4,5 +4,9 @@ from fourwiki.lib import try_me
 
 st.title('View some four wiki page')
 
-if st.button('HERE'):
-    print(try_me())
+click = st.button('HERE')
+
+if click:
+    result = try_me()
+
+print(result)
