f147b7473c564648500b990c63f08ffa530814dd
YiWen Hon <yiwench@gmail.com>
2022-02-22 09:14:42 +0000

    edited test

--- tests/test_four_wiki.py
+++ tests/test_four_wiki.py
@@ -1,5 +1,5 @@
 from fourwiki.lib import try_me
 
 def test_try_me():
-    result = try_me()
-    assert type(result) == str
+    result1, result2 = try_me()
+    assert type(result1 + result2) == str
